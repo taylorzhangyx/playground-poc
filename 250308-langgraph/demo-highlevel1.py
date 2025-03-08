42c96c9c0e154b62be9724bf8ad937c0ba2ab708
Taylor Zhang <taylor.zhang@booking.com>
2025-03-08 17:41:39 +0800

    add low level implementation
    Signed-off-by: Taylor Zhang <taylor.zhang@booking.com>

---
+++ 250308-langgraph/demo-highlevel1.py
@@ -0,0 +1,34 @@
+from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_anthropic import ChatAnthropic
+from langchain_core.tools import tool
+
+# Define the tools for the agent to use
+@tool
+def search(query: str):
+    """Call to surf the web."""
+    # This is a placeholder, but don't tell the LLM that...
+    if "sf" in query.lower() or "san francisco" in query.lower():
+        return "It's 60 degrees and foggy."
+    return "It's 900 degrees and sunny."
+
+
+tools = [search]
+model = ChatAnthropic(model="claude-3-5-sonnet-latest", temperature=0)
+
+# Initialize memory to persist state between graph runs
+checkpointer = MemorySaver()
+
+app = create_react_agent(model, tools, checkpointer=checkpointer)
+
+print("starting...")
+# Use the agent
+final_state = app.invoke(
+    {"messages": [{"role": "user", "content": "what is the weather in ams"}]},
+    config={"configurable": {"thread_id": 42}}
+)
+
+print("pending call")
+
+msg = final_state["messages"][-1].content
+print(msg)
